0714f6b612112a421a5b2da6a4c649a0a67c4f9c
GabrielGT01 <160685228+GabrielGT01@users.noreply.github.com>
2024-02-22 01:11:37 +0100

    Add files via upload
    uploaded documents without my created model

---
+++ streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from transformers import pipeline
+
+st.cache(ttl=24*3600, max_entries = 3)
+model = pipeline('translation',model = 'Helsinki-NLP/opus-mt-en-de')
+
+def main():
+    st.title("Translate short English text to German")
+    st.subheader("Powered by hugging face transformers created by Gabriel")
+
+    # Create an input text box
+    input_text = st.text_input("Enter your text", "")
+
+    # Create a button to trigger model inference
+    if st.button("Translate"):
+        # Perform inference using the loaded model
+        result = model(input_text)
+    
+        st.write("Translated text :", result[0]['translation_text'])
+
+if __name__ == "__main__":
+    main()
