671d5fa763ced22e5f349ec9e276ff12b86c8669
GabrielGT01 <160685228+GabrielGT01@users.noreply.github.com>
2024-02-22 18:27:20 +0100

    changes

--- streamlit.py
+++ streamlit.py
@@ -9,25 +9,24 @@ def main():
     st.title("Translation for short text to German")
     st.subheader("Powered by  transformers created by Gabriel")
 
-    
+    Languages = ["German to English", "English to German"]
 
-    Language = st.sidebar.selectbox(
-    "what language would you like to translate to"
+    choice = st.sidebar.selectbox("Language", Languages)
+    if choice == "German to English":
+        # Create an input text box
+        input_text = st.text_input("Enter your text", "")
 
-    ("German to English", "English to German")
-
-
-
-
-    # Create an input text box
-    input_text = st.text_input("Enter your text", "")
-
-    # Create a button to trigger model inference
-    if st.button("Translate"):
+        # Create a button to trigger model inference
+        if st.button("Translate"):
         # Perform inference using the loaded model
         result = model(input_text)
     
         st.write("Translated text :", result[0]['translation_text'])
+
+
+
+    else:
+        
 
 if __name__ == "__main__":
     main()
