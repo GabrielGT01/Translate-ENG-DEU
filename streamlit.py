e326b57fc1e35227b92c3e14fe7196d21ffed1a7
GabrielGT01 <160685228+GabrielGT01@users.noreply.github.com>
2024-02-22 18:31:54 +0100

    dual Languages

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,8 @@ from transformers import pipeline
 
 st.cache()
 
-model = pipeline('translation',model = 'Helsinki-NLP/opus-mt-en-de')
+eng-deu = pipeline('translation',model = 'Helsinki-NLP/opus-mt-en-de')
+deu-eng = pipeline('translation',model = 'Helsinki-NLP/opus-mt-de-en')
 
 def main():
     st.title("Translation for short text to German")
@@ -12,21 +13,32 @@ def main():
     Languages = ["German to English", "English to German"]
 
     choice = st.sidebar.selectbox("Language", Languages)
-    if choice == "German to English":
+
+    if choice == "English to German":
         # Create an input text box
         input_text = st.text_input("Enter your text", "")
 
         # Create a button to trigger model inference
         if st.button("Translate"):
+
         # Perform inference using the loaded model
-        result = model(input_text)
-    
-        st.write("Translated text :", result[0]['translation_text'])
+           result = eng-deu(input_text)
+           st.write("Translated text :", result[0]['translation_text'])
 
 
 
     else:
-        
+        input_text = st.text_input("Enter your text", "")
+
+        # Create a button to trigger model inference
+        if st.button("Translate"):
+
+        # Perform inference using the loaded model
+           result = deu-eng(input_text)
+           st.write("Translated text :", result[0]['translation_text'])
+
+
+
 
 if __name__ == "__main__":
     main()
